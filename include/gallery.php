1c2c7a5570fc74dbfe3c1e8da502267cb19737ba
YESPC-05\Yes Enterprise <jennelynporazo92@gmail.com>
2018-05-09 19:16:37 +0800

    I hate this one.

---
+++ include/gallery.php
@@ -0,0 +1,120 @@
+<html>
+<head>
+	<title>Century 21 Platinum</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="plugins/slick/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="plugins/slick/slick/slick-theme.css">
+	<link rel="stylesheet" href="plugins/css/bootstrap.min.css" >
+  	<link rel="stylesheet" href="plugins/css/gallery1.css">
+</head>
+<body>
+	<div class="gallery1">
+		<div class="container">
+			<div class="flex-parent">
+				<div class="flex1">
+					<p class="guide">OUR HAND PICKED AGENTS ARE TO GUIDE & HELP YOU</p>
+				</div>
+				<div class="flex1">
+					<p class="agent">PROFESSIONAL GUIDANCE FROM TOP AGENTS</p>
+				</div>
+				<div class="flex1">
+					<div class="sub-parent">
+						<div class="flex2" style="background-image:url('Pix/img/img5.png')">
+							<!-- <p class="tnega">AGENT I <br /><span class="real">Realtor</span></p> -->
+							<div class="flex3" style="background-image:url('Pix/img/img5.png')">
+								<div class="view">
+									<p>VIEW DETAILS</p>
+								</div>
+								<div class="des">
+									<h4>ASHLEY CRUZ</h4>	
+									<p>Realtor <span class="glyphicon glyphicon-phone">0921-258-7541</span></p>
+								</div>
+							</div>
+						</div>
+						<div class="flex2"style="background-image:url('Pix/img/img4.png')">
+							<p class="tnega">AGENT II <br /><span class="real">Realtor</span></p>
+							<div class="flex3" style="background-image:url('Pix/img/img4.png')">
+								<div class="view">
+									<p>VIEW DETAILS</p>
+								</div>
+								<div class="des">
+									<h4>KATRINA DE GUZMAN</h4>	
+									<p>Realtor <span class="glyphicon glyphicon-phone">0909-040-0545</span></p>
+								</div>
+							</div>
+						</div>
+						<div class="flex2"style="background-image:url('Pix/img/img6.png')">
+							<p class="tnega">AGENT III <br /><span class="real">Realtor</span></p>
+							<div class="flex3" style="background-image:url('Pix/img/img6.png')">
+								<div class="view">
+									<p>VIEW DETAILS</p>
+								</div>
+								<div class="des">
+									<h4>PRINCESS YELSHA</h4>	
+									<p>Realtor <span class="glyphicon glyphicon-phone">0938-444-4331</span></p>
+								</div>
+							</div>
+						</div>
+						<div class="flex2"style="background-image:url('Pix/img/img7.png')">
+							<p class="tnega">AGENT IV <br /><span class="real">Realtor</span></p>
+							<div class="flex3" style="background-image:url('Pix/img/img7.png')">
+								<div class="view">
+									<p>VIEW DETAILS</p>
+								</div>
+								<div class="des">
+									<h4>MICHIN XOU</h4>	
+									<p>Realtor <span class="glyphicon glyphicon-phone">0956-653-6891</span></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="plugins/jquery/jquery.min.js"></script>
+	<script src="plugins/js/bootstrap.min.js"></script>
+	<script src="plugins/slick/slick/slick.min.js"></script>
+
+
+	<script type="text/javascript">
+	$('.sub-parent').slick({
+	  dots: false,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        dots: false
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
+	});	
+	</script>
+</body>
+</html>
---
+++ include/slider.php
@@ -0,0 +1,136 @@
+<html>
+<head>
+	<title>Century 21 Platinum</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="plugins/slick/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="plugins/slick/slick/slick-theme.css">
+	<link rel="stylesheet" href="plugins/css/bootstrap.min.css" >
+	<link rel="stylesheet" href="plugins/css/final.css">
+
+</head>
+<body>
+	<div id="gallery">
+		<div class="container">
+			<div class="text-center">
+				<p class="care">CAREFULLY PICKED HIGH END PROPERTIES</p>
+				<p class="most">MOST RECENT PROPERTY OFFERS FOR YOU</p>
+			</div>
+			<div class="flex-parent">
+				<div class="flex1">
+					<div class="flex2">
+						<img src="Pix/img/img2.png"/>
+						<div class="con">
+							<p class="con1">123 MAIN STREET, EDMONTON,AB<br/>6524, Philippines<br/>100 sq ft.</p>
+							<div class="view">
+								<p>VIEW DETAILS</p>
+								<div class="tri"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex1">
+					<div class="flex2">
+						<img src="Pix/img/img3.png"/>
+						<div class="con">
+							<p class="con1">123 MAIN STREET, EDMONTON,AB<br/>6524, Philippines<br/>50 sq ft.</p>
+							<div class="view">
+								<p>VIEW DETAILS</p>
+								<div class="tri"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex1">
+					<div class="flex2">
+						<img src="Pix/img/img1.png"/>
+						<div class="con">
+							<p class="con1">123 MAIN STREET, EDMONTON,AB<br/>6524, Philippines<br/>50 sq ft.</p>
+							<div class="view">
+								<p>VIEW DETAILS</p>
+								<div class="tri"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex1">
+					<div class="flex2">
+						<img src="Pix/img/img2.png"/>
+						<div class="con">
+							<p class="con1">123 MAIN STREET, EDMONTON,AB<br/>6524, Philippines<br/>100 sq ft.</p>
+							<div class="view">
+								<p>VIEW DETAILS</p>
+								<div class="tri"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex1">
+					<div class="flex2">
+						<img src="Pix/img/img3.png"/>
+						<div class="con">
+							<p class="con1">123 MAIN STREET, EDMONTON,AB<br/>6524, Philippines<br/>50 sq ft.</p>
+							<div class="view">
+								<p>VIEW DETAILS</p>
+								<div class="tri"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex1">
+					<div class="flex2">
+						<img src="Pix/img/img1.png"/>
+						<div class="con">
+							<p class="con1">123 MAIN STREET, EDMONTON,AB<br/>6524, Philippines<br/>50 sq ft.</p>
+							<div class="view">
+								<p>VIEW DETAILS</p>
+								<div class="tri"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="plugins/jquery/jquery.min.js"></script>
+	<script src="plugins/js/bootstrap.min.js"></script>
+	<script src="plugins/slick/slick/slick.min.js"></script>
+
+	</script>
+	<script type="text/javascript">
+	var $slickEl = $('.flex-parent');
+	$slickEl.slick({
+		centerMode: true,
+		centerPadding: '0',
+		slidesToShow: 3,
+		focusOnSelect: true,
+		autoplay: true,
+	 	autoplaySpeed: 2000,	  
+	  	dots: false,
+		infinite: true,
+		responsive: [
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        arrows: false,
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        arrows: false,
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 1
+		      }
+		    }
+		]
+	});
+	</script>
+</body>
+</html>
+	
