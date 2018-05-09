0745c380f8e1e6363589dbd06ffbfc57575cecde
YESPC-06\YESCOM-2 <jbavellana39@gmail.com>
2018-05-09 19:02:53 +0800

    footer nani

--- include/header.php
+++ include/header.php
@@ -7,8 +7,7 @@
 
   <link rel="stylesheet" type="text/css" href="assets/plugins/slick/slick/slick.css">
   <link rel="stylesheet" type="text/css" href="assets/plugins/slick/slick/slick-theme.css">
-  <link rel="stylesheet" type="text/css" href="assets/contents/font-awesome/css/font-awesome.css">
-  <link rel="stylesheet" type="text/css" href="assets/contents/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/plugins/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css" >
   <link rel="stylesheet" href="assets/contents/css/test.css">
   <link rel="stylesheet" href="assets/contents/css/index.css">
