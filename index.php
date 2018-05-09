d7c33f909e8b1f1c3b47bfdc8d1ad87b195e7525
marnidevlabs <31850775+marnidevlabs@users.noreply.github.com>
2018-05-09 17:59:59 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+  include 'include/header.php';
+  include 'include/banner.php';
+  include 'include/slider.php';
+  include 'include/aboutus.php';
+  include 'include/gallery.php';
+  include 'include/testimonials.php';
+  include 'include/serve.php';
+  include 'include/footer.php';
+?>
